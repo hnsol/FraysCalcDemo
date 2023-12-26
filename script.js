57058399b1603b97309d05f3a6856030860b9414
hnsol <masatora.bd5@gmail.com>
2023-12-26 23:59:36 +0900

    test upload

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-document.getElementById('testButton').addEventListener('click', function() {
-    document.getElementById('message').textContent = 'ボタンがクリックされました！';
-});
+function calculate() {
+    var quantity = document.getElementById('quantity').value;
+    document.getElementById('oil').textContent = (10 / 50 * quantity).toFixed(2);
+    document.getElementById('neri').textContent = (13.28 / 50 * quantity).toFixed(2);
+    document.getElementById('goma').textContent = (6.6 / 50 * quantity).toFixed(2);
+    document.getElementById('flour').textContent = (40 / 50 * quantity).toFixed(2);
+    document.getElementById('sugar').textContent = (10 / 50 * quantity).toFixed(2);
+    document.getElementById('salt').textContent = (0.1 / 50 * quantity).toFixed(2);
+}
 
