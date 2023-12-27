a742c57cfcb88e4114284eed188d05e05d7a70a0
hnsol <masatora.bd5@gmail.com>
2023-12-27 23:28:31 +0900

    chore: changed parameters

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ document.getElementById('quantity').addEventListener('input', calculate);
 
 function calculate() {
     var quantity = document.getElementById('quantity').value;
-    document.getElementById('oil').textContent = (10 / 50 * quantity).toFixed(2);
-    document.getElementById('neri').textContent = (13.28 / 50 * quantity).toFixed(2);
-    document.getElementById('goma').textContent = (6.6 / 50 * quantity).toFixed(2);
-    document.getElementById('flour').textContent = (40 / 50 * quantity).toFixed(2);
-    document.getElementById('sugar').textContent = (10 / 50 * quantity).toFixed(2);
-    document.getElementById('salt').textContent = (0.1 / 50 * quantity).toFixed(2);
+    document.getElementById('oil').textContent =   (10   / 60 * quantity).toFixed(2);
+    document.getElementById('neri').textContent =  (16.6 / 60 * quantity).toFixed(2);
+    document.getElementById('goma').textContent =  (6.6  / 60 * quantity).toFixed(2);
+    document.getElementById('flour').textContent = (40   / 60 * quantity).toFixed(2);
+    document.getElementById('sugar').textContent = (10   / 60 * quantity).toFixed(2);
+    document.getElementById('salt').textContent =  (0.1  / 60 * quantity).toFixed(2);
 
     document.getElementById('materialsTitle').textContent = quantity + '個に対する材料';
 
